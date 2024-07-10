dec448c97711fc14ac40240ec74422136ca0619b
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-07-10 09:50:12 +0530

    get user by id

--- demo/src/main/java/com/example/demo/repo/UserRepo.java
+++ demo/src/main/java/com/example/demo/repo/UserRepo.java
@@ -2,8 +2,12 @@ package com.example.demo.repo;
 
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
+
+    @Query(value = "SELECT * FROM User WHERE id = ?1", nativeQuery = true)
+    User getUserById(Integer userId);
 }
